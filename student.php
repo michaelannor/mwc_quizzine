738c13896a26068879a7026a8cd419acce6755cc
michaelannor <m.annor13@gmail.com>
2015-11-22 21:19:17 +0000

    add student function complete

--- student.php
+++ student.php
@@ -10,6 +10,17 @@ include_once ("adb.php");
 
 class student extends adb {
 
+  /**
+   * description: The add_student function adds a student record
+   * @param varchar $username the users username for login
+   * @param varchar $password the users password for login
+   * @param varchar $parent the user's parent's username
+   */
+    function add_student($username, $password, $parent){
+      $str_query="insert into mwc_quizzine_student set username='$username',password='$password',
+        parent='$parent'";
+        return $this->query($str_query);
+    }
 
 
 }
