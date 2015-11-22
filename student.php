1f702e0a8b9f2548884417a6646ea32b8e2f1581
michaelannor <m.annor13@gmail.com>
2015-11-22 21:08:32 +0000

    started work on the student class

---
+++ student.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * author: Michael Annor
+ * date: 22nd November, 2015
+ * description: Class with queries to access the mwc_quizzine_student table
+ */
+
+include_once ("adb.php");
+
+class student extends adb {
+
+
+
+}
+
+
+?>
